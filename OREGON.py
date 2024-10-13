482586bd02e2066620dc30244d9d693a9ae23e4e
MickeySauce68 <103530779+MickeySauce68@users.noreply.github.com>
2024-10-12 23:27:26 -0400

    Add files via upload

--- OREGON.py
+++ OREGON.py
@@ -1,6 +1,11 @@
+from goto import with_goto
 import random
 import time
-from goto import with_goto
+import random
+import threading
+global sound
+global pressed
+
 
 
 @with_goto
@@ -491,21 +496,51 @@ def main():
 
     #***HUNTING*** ########### LINE 2530
     label .two540
-    if B>39:
-        goto .two570
+    def get_user_input(sound, input_ready):
+        """Gets user input while the timer is running."""
+        pressed = input("TYPE " + sound + ": ")
+        input_ready.append(pressed)
 
-    print("TOUGH---YOU NEED MORE BULLETS TO GO HUNTING") ###LINE 2550
+    def check(sound, pressed):
+        """Checks if the user's input matches the sound."""
+        if pressed and sound == pressed[0]:
+            print("DONE")
+        else:
+            print("WRONG! The correct sound was: " + sound)
 
-    goto .two080 #######MAY NEED TO CHANGE :( ###############################**********************************************<-----
+    def six140():
+        """Main game function."""
+        rand = random.randint(0, 3)
+        gun_sounds = ["BANG", "BLAM", "POW", "WHAM"]
+        sound = gun_sounds[rand]
 
-    M=M-45
 
-    def six140(): ######### -----------> START <-------------
-         rand = random.randint(1,4)
+        # Create a list to store user input
+        input_ready = []
+
+
+        # Get user input in the main thread
+        get_user_input_thread = threading.Thread(target=get_user_input, args=(sound, input_ready))
+        get_user_input_thread.start()
+
+        # Wait for both threads to finish
+        get_user_input_thread.join()
+
+        # Check the input after both threads are done
+        check(sound, input_ready)
+
+    six140()
+
+    
         
 
-    if B1<=1:
-        goto .two660
+    
+
+
+
+
+
+    
 
 
 
